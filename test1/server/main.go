7f6dcf3e0e10c43358eec037cd03016d44e2105b
Francisco Diaz <fran.diaz.rio11@gmail.com>
2020-11-23 17:07:41 -0400

    Initial Commit

--- test1/server/main.go
+++ test1/server/main.go
@@ -5,15 +5,12 @@ import (
 	"fmt"
 	pb "mygrpc/messages"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-
-const (
-	port = ":50051"
-)
 
 
 type server struct{
@@ -31,12 +28,19 @@ func (s *server) EchoReply(ctx context.Context, reply *pb.Reply)(*pb.Reply, erro
 }
 
 func main(){
-	socket, err :=	net.Listen("tcp", port)
+	arguments := os.Args
 
+	if len(arguments)==1{
+		fmt.Println("Invalid: Enter port number")
+	}
+	
+	port := ":"+arguments[1]
+	socket, err :=	net.Listen("tcp", port)
 	if err != nil{
 		fmt.Println(err)
 	}
 	
+	defer socket.Close()
 	fmt.Println("Starting grpc server at "+port)
 	s := grpc.NewServer()
 	pb.RegisterAgentServer(s, &server{})
